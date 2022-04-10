b116274017c63ef56b2a3100da31eea0c2d388c8
ArtRegina <reginalefler@yandex.ru>
2022-04-10 23:19:27 +0300

    button click

--- app.js
+++ app.js
@@ -5,6 +5,8 @@ const todoList = document.querySelector(".todo-list");
 
 //Event Listeners
 todoButton.addEventListener("click", addToo);
+todoList.addEventListener("click", deleteCheck);
+
 
 //Functions
 
@@ -33,4 +35,18 @@ function addToo(event) {
     todoList.appendChild(todoDiv);  
     //Clear Todo INPUT VLUE
     todoInput.value = "";  
+}
+function deleteCheck(e) {
+    const item = e.target;
+    //DELETE TODO
+    if (item.classList[0] === "trash-btn"){
+        const todo = item.parentElement;
+        todo.remove();
+    }
+
+    //CHECK MARK
+    if (item.classList[0] === "complete-btn"){
+        const todo = item.parentElement;
+        todo.classList.toggle("completed")
+    }
 }
