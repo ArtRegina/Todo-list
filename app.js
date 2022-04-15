49d45424943cb95b721c1a8be17e71d7f2ca3dc0
ArtRegina <reginalefler@yandex.ru>
2022-04-16 00:20:10 +0300

    buttons works

--- app.js
+++ app.js
@@ -2,11 +2,12 @@
 const todoInput = document.querySelector(".todo-input");
 const todoButton = document.querySelector(".todo-button");
 const todoList = document.querySelector(".todo-list");
+const filterOption = document.querySelector(".filter-todo");
 
 //Event Listeners
 todoButton.addEventListener("click", addToo);
 todoList.addEventListener("click", deleteCheck);
-
+filterOption.addEventListener("click", filterTodo);
 
 //Functions
 
@@ -50,8 +51,33 @@ function deleteCheck(e) {
     }
 
     //CHECK MARK
-    if (item.classList[0] === "complete-btn"){
+    if (item.classList[0] === "complete-btn") {
         const todo = item.parentElement;
-        todo.classList.toggle("completed")
+        todo.classList.toggle("completed");
     }
+}
+
+function filterTodo(e) {
+    const todos = todoList.childNodes;
+    todos.forEach(function(todo) {
+        switch (e.target.value) {
+            case "all":
+                todo.style.display = "flex";
+                break;
+                case "completed":
+                    if (todo.classList.contains("completed")) {
+                        todo.style.display = "flex";
+                    } else {
+                        todo.style.display = "none";
+                    }
+                    break;
+                    case "uncompleted":
+                        if(!todo.classList.contains("completed")) {
+                            todo.style.display = "flex";
+                        } else {
+                            todo.style.display = "none";
+                        }
+                        break;
+        }
+    });
 }
